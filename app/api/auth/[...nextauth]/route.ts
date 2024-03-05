b6d47327954df583a43247a9ad3bc97602701e15
Abrão Rodrigues <arnf2710@gmail.com>
2024-03-05 16:09:17 -0300

    Implements working auth skeleton

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        username: {label: 'Username', type: 'text', placeholder: 'your username'},
+        password: {label: 'Passowrd', type: 'password', placeholder: 'your password'},
+
+      },
+      authorize(credentials) {
+        if(credentials && credentials['username'] === 'teste' && credentials['password'] === '123456') {
+          return {
+            id: 'brones',
+            name: 'abrao',
+          }
+        }
+
+        return null;
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 60,
+  }
+});
+
+export { handler as GET, handler as POST};
